ba0e1fb91aa850fced5c6efdcd2e4404417f05c3
albshady <3litac@gmail.com>
2024-02-29 13:31:31 +0100

    🧑‍💻 Add turned-off gitlab lsp plugin

--- plugins/user.lua
+++ plugins/user.lua
@@ -28,4 +28,17 @@ return {
     end,
     lazy = false,
   },
+  -- {
+  --   'git@gitlab.com:gitlab-org/editor-extensions/gitlab.vim.git',
+  --   event = { 'BufReadPre', 'BufNewFile' }, -- Activate when a file is created/opened
+  --   ft = { 'python', 'rust' }, -- Activate when a supported filetype is open
+  --   cond = function()
+  --     return vim.env.GITLAB_TOKEN ~= nil and vim.env.GITLAB_TOKEN ~= '' -- Only activate is token is present in environment variable (remove to use interactive workflow)
+  --   end,
+  --   opts = {
+  --     statusline = {
+  --       enabled = false, -- Hook into the builtin statusline to indicate the status of the GitLab Duo Code Suggestions integration
+  --     },
+  --   },
+  -- },
 }
