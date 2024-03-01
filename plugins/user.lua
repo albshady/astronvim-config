be49c51d33578321e5629168e9512d8530a24604
albshady <3litac@gmail.com>
2024-03-01 14:10:43 +0100

    💄 Add transparent-nvim plugin

--- plugins/user.lua
+++ plugins/user.lua
@@ -28,6 +28,19 @@ return {
     end,
     lazy = false,
   },
+  {
+    -- https://github.com/AstroNvim/astrocommunity/blob/dc7ea3b358eafd508261b8108a37dc3ca11b414e/lua/astrocommunity/utility/transparent-nvim/init.lua
+    "xiyaowong/transparent.nvim",
+    lazy = false,
+    keys = {
+      { "<leader>uT", "<cmd>TransparentToggle<CR>", desc = "Toggle transparency" },
+    },
+    config = function()
+      require("notify").setup({
+        background_colour = "#000000",
+      })
+    end,
+  },
   -- {
   --   'git@gitlab.com:gitlab-org/editor-extensions/gitlab.vim.git',
   --   event = { 'BufReadPre', 'BufNewFile' }, -- Activate when a file is created/opened
