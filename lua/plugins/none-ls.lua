e1f222ee49f66e6c70a18251e3c9b8968b9c42a2
albshady <3litac@gmail.com>
2024-05-23 01:33:34 +0200

    🔧 Configure none-ls to use ruff when needed

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,22 +1,44 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
--- Customize None-ls sources
+local function file_contains_word(filepath, word)
+  local file = io.open(filepath, "r")
+  if file then
+    local content = file:read("*all")
+    file:close()
+    if content:find(word) then
+      return true
+    end
+  end
+  return false
+end
 
 ---@type LazySpec
 return {
   "nvimtools/none-ls.nvim",
+  dependencies = {
+    "nvimtools/none-ls-extras.nvim",
+  },
   opts = function(_, config)
-    -- config variable is the default configuration table for the setup function call
-    -- local null_ls = require "null-ls"
+    local null_ls = require("null-ls")
 
-    -- Check supported formatters and linters
-    -- https://github.com/nvimtools/none-ls.nvim/tree/main/lua/null-ls/builtins/formatting
-    -- https://github.com/nvimtools/none-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
-    config.sources = {
-      -- Set a formatter
-      -- null_ls.builtins.formatting.stylua,
-      -- null_ls.builtins.formatting.prettier,
-    }
+    -- Determine whether to use ruff based on pyproject.toml
+    local project_root = vim.fn.getcwd()
+    local pyproject_path = project_root .. "/pyproject.toml"
+    local use_ruff = file_contains_word(pyproject_path, "ruff")
+
+    -- Configure sources based on the presence of ruff in pyproject.toml
+    if use_ruff then
+      config.sources = {
+        null_ls.builtins.formatting.black,
+        require("none-ls.formatting.ruff"),
+        require("none-ls.diagnostics.ruff"),
+      }
+    else
+      config.sources = {
+        null_ls.builtins.formatting.isort,
+        null_ls.builtins.formatting.black,
+        require("none-ls.diagnostics.flake8"),
+      }
+    end
+
     return config -- return final config table
   end,
 }
