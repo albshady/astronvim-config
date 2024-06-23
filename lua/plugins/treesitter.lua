dd685a22d84f624cb737699a9e742e7e9f744a51
albshady <3litac@gmail.com>
2024-06-23 15:10:38 +0200

    💄 Disable TS highlight for xml, json, yaml, toml

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,16 +1,19 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
--- Customize Treesitter
-
 ---@type LazySpec
 return {
   "nvim-treesitter/nvim-treesitter",
-  opts = function(_, opts)
-    -- add more things to the ensure_installed table protecting against community packs modifying it
-    opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
-      "lua",
-      "vim",
-      -- add more arguments for adding more treesitter parsers
-    })
-  end,
+  opts = {
+    ensure_installed = { "python" },
+    highlight = {
+      enable = true,
+      disable = {
+        "yaml",
+        "toml",
+        "xml",
+        "json",
+      },
+    },
+    indent = {
+      enable = true,
+    },
+  },
 }
